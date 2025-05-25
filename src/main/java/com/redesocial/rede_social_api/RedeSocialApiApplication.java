0c937e5b68d33c5adc88742cd6f5b17a1c9f9b18
felipe67r <feliperibeiro67r@gmail.com>
2025-05-25 14:28:22 -0300

    Adicionando dependências

---
+++ src/main/java/com/redesocial/rede_social_api/RedeSocialApiApplication.java
@@ -0,0 +1,13 @@
+package com.redesocial.rede_social_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedeSocialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedeSocialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redesocial/rede_social_api/RedeSocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redesocial.rede_social_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
